43d7c36084de47e147bf9684f610da129ba9ae0c
HungHcc <buisihung321@gmail.com>
2021-12-03 08:38:47 +0700

    test husky, lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { ProgressBarStyle } from './components/LoadingScreen';
 import ThemePrimaryColor from './components/ThemePrimaryColor';
 
 // ----------------------------------------------------------------------
-
+const test = 'a';
 export default function App() {
   return (
     <ThemeConfig>
