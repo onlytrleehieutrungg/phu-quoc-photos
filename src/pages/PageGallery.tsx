65fb34ac713194b742be6c5f019d1768b7aaba86
khoa-la <lakhoa2806@gmail.com>
2021-12-20 16:33:01 +0700

    feat: share page and download all images

--- src/pages/PageGallery.tsx
+++ src/pages/PageGallery.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import Box from '@mui/material/Box';
 import Page from '../components/Page';
 import ImageList from '@mui/material/ImageList';
@@ -248,17 +249,12 @@ export default function PageGallery() {
             <Divider>
               <Chip label="Chia sẻ" />
             </Divider>
-            <IconButton
-              sx={{ color: 'blue' }}
-              className="fb-share-button"
-              data-href="https://developers.facebook.com/docs/plugins/"
-              data-layout="button"
-              data-size="large"
-            >
+            <IconButton>
+              {/* https://www.facebook.com/sharer/sharer.php?u=${window.location.href} */}
               <a
+                href={`https://www.facebook.com/sharer/sharer.php?u=https://stg.phuquocphoto.com/kho-anh?ma-don-hang=order1`}
                 target="_blank"
-                href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-                className="fb-xfbml-parse-ignore"
+                rel="noreferrer"
               >
                 <FacebookIcon />
               </a>
