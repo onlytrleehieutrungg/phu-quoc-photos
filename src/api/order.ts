9de7bcf44c389c4e5917a8c6cd36cf37e7388572
onlytrleehieutrungg <trungtran2k01@gmail.com>
2022-02-28 10:07:49 +0700

    fix: get album by phone number

--- src/api/order.ts
+++ src/api/order.ts
@@ -2,8 +2,8 @@ import { TOrderDetail, TOrder } from 'types/order';
 
 import request from './axios';
 
-const getOrderDetail = (orderId: any) => request.get<TOrderDetail>(`orders/${orderId}`);
-const getOrder = (orderId: any) => request.get<TOrder>(`orders/${orderId}`);
+const getOrderDetail = (orderId: any) => request.get<TOrderDetail>(`orders/phone/${orderId}`);
+const getOrder = (orderId: any) => request.get<TOrder>(`orders/phone/${orderId}`);
 
 const orderApi = {
   getOrderDetail,
